a9499a9408d97a27895dfa28147ceb4c01e844d7
Christian Voß <cech12@gmail.com>
2021-01-15 23:25:57 +0100

    - increase default cook time factor to 4 times (4 times the time of smoking recipes, twice the time of normal smelting)

--- src/main/java/cech12/solarcooker/config/ServerConfig.java
+++ src/main/java/cech12/solarcooker/config/ServerConfig.java
@@ -29,7 +29,7 @@ public class ServerConfig {
                 .define("vanillaRecipeType", "smoking");
         COOK_TIME_FACTOR = builder
                 .comment("Cook time factor of the solar cooker in relation to corresponding vanilla furnace. (i. e. 0.5 - half the time, 1.0 same time, 2.0 twice the time)")
-                .defineInRange("cookTimeFactor", 1.0, 0.0, 100.0);
+                .defineInRange("cookTimeFactor", 4.0, 0.0, 100.0);
         RECIPE_BLACKLIST = builder
                 .comment("A comma separated list of all vanilla recipes that should not be used by the solar cooker. Example: \"baked_potato,baked_potato_from_smoking,othermod:other_baked_food\"")
                 .define("recipeBlacklist", "");
