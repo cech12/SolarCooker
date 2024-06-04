49d65c6d1c364cd5de90e3acb7ff53f5f332725b
cech12 <cech12@gmail.com>
2024-06-04 20:04:28 +0200

    added Roughly Enough Items (REI) support

---
+++ fabric/src/main/java/de/cech12/solarcooker/rei/SolarCookingReiDisplayCategory.java
@@ -0,0 +1,68 @@
+package de.cech12.solarcooker.rei;
+
+import com.google.common.collect.Lists;
+import de.cech12.solarcooker.Constants;
+import me.shedaniel.math.Point;
+import me.shedaniel.math.Rectangle;
+import me.shedaniel.rei.api.client.gui.DisplayRenderer;
+import me.shedaniel.rei.api.client.gui.Renderer;
+import me.shedaniel.rei.api.client.gui.SimpleDisplayRenderer;
+import me.shedaniel.rei.api.client.gui.widgets.Widget;
+import me.shedaniel.rei.api.client.gui.widgets.Widgets;
+import me.shedaniel.rei.api.client.registry.display.DisplayCategory;
+import me.shedaniel.rei.api.common.category.CategoryIdentifier;
+import me.shedaniel.rei.api.common.entry.EntryStack;
+import me.shedaniel.rei.api.common.entry.type.VanillaEntryTypes;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.ItemStack;
+
+import java.text.DecimalFormat;
+import java.util.Collections;
+import java.util.List;
+
+public class SolarCookingReiDisplayCategory implements DisplayCategory<SolarCookingReiDisplay> {
+
+    public static final CategoryIdentifier<SolarCookingReiDisplay> ID = CategoryIdentifier.of(Constants.MOD_ID, Constants.SOLAR_COOKING_NAME);
+
+    @Override
+    public CategoryIdentifier<? extends SolarCookingReiDisplay> getCategoryIdentifier() {
+        return ID;
+    }
+
+    @Override
+    public Component getTitle() {
+        return Component.translatable("block.solarcooker.solar_cooker");
+    }
+
+    @Override
+    public Renderer getIcon() {
+        return EntryStack.of(VanillaEntryTypes.ITEM, new ItemStack(Constants.SOLAR_COOKER_ITEM.get()));
+    }
+
+    @Override
+    public int getDisplayHeight() {
+        return 49;
+    }
+
+    @Override
+    public DisplayRenderer getDisplayRenderer(SolarCookingReiDisplay display) {
+        return SimpleDisplayRenderer.from(Collections.singletonList(display.getInputEntries().get(0)), display.getOutputEntries());
+    }
+
+    @Override
+    public List<Widget> setupDisplay(SolarCookingReiDisplay display, Rectangle bounds) {
+        Point startPoint = new Point(bounds.getCenterX() - 41, bounds.y + 10);
+        double cookingTime = display.getCookTime();
+        DecimalFormat df = new DecimalFormat("###.##");
+        List<Widget> widgets = Lists.newArrayList();
+        widgets.add(Widgets.createRecipeBase(bounds));
+        widgets.add(Widgets.createResultSlotBackground(new Point(startPoint.x + 61, startPoint.y + 9)));
+        widgets.add(Widgets.createBurningFire(new Point(startPoint.x + 1, startPoint.y + 20)).animationDurationMS(10000.0));
+        widgets.add(Widgets.createLabel(new Point(bounds.x + bounds.width - 5, bounds.y + 5), Component.translatable("category.rei.cooking.time&xp", df.format(display.getXp()), df.format(cookingTime / 20.0))).noShadow().rightAligned().color(-12566464, -4473925));
+        widgets.add(Widgets.createArrow(new Point(startPoint.x + 24, startPoint.y + 8)).animationDurationTicks(cookingTime));
+        widgets.add(Widgets.createSlot(new Point(startPoint.x + 61, startPoint.y + 9)).entries(display.getOutputEntries().get(0)).disableBackground().markOutput());
+        widgets.add(Widgets.createSlot(new Point(startPoint.x + 1, startPoint.y + 1)).entries(display.getInputEntries().get(0)).markInput());
+        return widgets;
+    }
+
+}
---
+++ forge/src/main/java/de/cech12/solarcooker/rei/SolarCookingReiDisplay.java
@@ -0,0 +1,62 @@
+package de.cech12.solarcooker.rei;
+
+import de.cech12.solarcooker.crafting.SolarCookingRecipe;
+import de.cech12.solarcooker.platform.Services;
+import me.shedaniel.rei.api.common.category.CategoryIdentifier;
+import me.shedaniel.rei.api.common.display.basic.BasicDisplay;
+import me.shedaniel.rei.api.common.entry.EntryIngredient;
+import me.shedaniel.rei.api.common.registry.RecipeManagerContext;
+import me.shedaniel.rei.api.common.util.EntryIngredients;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.item.crafting.AbstractCookingRecipe;
+import net.minecraft.world.item.crafting.RecipeHolder;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+public class SolarCookingReiDisplay extends BasicDisplay {
+
+    private final float xp;
+    private final double cookTime;
+
+    public <T extends AbstractCookingRecipe> SolarCookingReiDisplay(RecipeHolder<T> recipe) {
+        this(EntryIngredients.ofIngredients(
+                recipe.value().getIngredients()),
+                Collections.singletonList(EntryIngredients.of(recipe.value().getResultItem(null))),
+                recipe,
+                recipe.value().getExperience(),
+                recipe.value() instanceof SolarCookingRecipe ? recipe.value().getCookingTime() : (recipe.value().getCookingTime() * Services.CONFIG.getCookTimeFactor()));
+    }
+
+    public SolarCookingReiDisplay(List<EntryIngredient> input, List<EntryIngredient> output, CompoundTag tag) {
+        this(input, output, RecipeManagerContext.getInstance().byId(tag, "location"),
+                tag.getFloat("xp"), tag.getDouble("cookTime"));
+    }
+
+    public SolarCookingReiDisplay(List<EntryIngredient> input, List<EntryIngredient> output, RecipeHolder<?> recipe, float xp, double cookTime) {
+        super(input, output, Optional.ofNullable(recipe).map(RecipeHolder::id));
+        this.xp = xp;
+        this.cookTime = cookTime;
+    }
+
+    public static Serializer<SolarCookingReiDisplay> getSerializer() {
+        return BasicDisplay.Serializer.ofRecipeLess(SolarCookingReiDisplay::new, (display, tag) -> {
+            tag.putFloat("xp", display.xp);
+            tag.putDouble("cookTime", display.cookTime);
+        });
+    }
+
+    @Override
+    public CategoryIdentifier<?> getCategoryIdentifier() {
+        return SolarCookingReiDisplayCategory.ID;
+    }
+
+    public double getCookTime() {
+        return cookTime;
+    }
+
+    public float getXp() {
+        return xp;
+    }
+}
