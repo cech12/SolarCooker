ca145daacfa4552661cbe393239d6730d7a9ab15
cech12 <cech12@gmail.com>
2025-02-22 21:01:34 +0100

    - fixed: solar cooker lid was moving when a recipe was finished although a next recipe is available - fixed: solar cooker opening sound was played after world loading after an active recipe was finished although a next recipe is available - fixed: item names were not displayed correctly

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,7 +3,14 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/) and this project adheres to [Forge Recommended Versioning](https://mcforge.readthedocs.io/en/latest/conventions/versioning/).
 
+## [1.21.3-4.3.0.1] - 2024-02-22
+### Fixed
+- solar cooker lid was moving when a recipe was finished although a next recipe is available
+- solar cooker opening sound was played after world loading after an active recipe was finished although a next recipe is available
+- item names were not displayed correctly
+
 ## [1.21.3-4.3.0.0] - 2024-02-19
+### Changed
 - Updated to Minecraft 1.21.3 (Fabric 0.107.0+1.21.3, NeoForge 21.3.56, Forge 53.0.47)
 - Updated Cloth Config support (16.0.141) (Fabric/Quilt)
 - Updated ModMenu support (12.0.0) (Fabric/Quilt)
--- common/src/main/java/de/cech12/solarcooker/blockentity/SolarCookerBlockEntity.java
+++ common/src/main/java/de/cech12/solarcooker/blockentity/SolarCookerBlockEntity.java
@@ -80,6 +80,7 @@ public class SolarCookerBlockEntity extends BaseContainerBlockEntity implements 
     private final ContainerOpenersCounter openersCounter;
     private final ChestLidController chestLidController;
     private boolean isLidOpen = false;
+    private boolean isRecipeActive = false;
     private final Object2IntOpenHashMap<ResourceKey<Recipe<?>>> usedRecipes = new Object2IntOpenHashMap<>();
 
     public SolarCookerBlockEntity(BlockEntityType<?> tileTypeIn, BlockPos pos, BlockState state,
@@ -214,7 +215,12 @@ public class SolarCookerBlockEntity extends BaseContainerBlockEntity implements 
         ContainerHelper.loadAllItems(compound, this.items, provider);
         this.cookTime = compound.getInt("CookTime");
         this.cookTimeTotal = compound.getInt("CookTimeTotal");
-        updateShouldLidBeOpen(this.openersCounter.getOpenerCount());
+        this.isRecipeActive = compound.getBoolean("IsRecipeActive");
+        if (this.level == null) { //at server initialisation
+            this.isLidOpen = this.isRecipeActive;
+        } else {
+            updateShouldLidBeOpen(this.openersCounter.getOpenerCount());
+        }
     }
 
     @Override
@@ -222,6 +228,7 @@ public class SolarCookerBlockEntity extends BaseContainerBlockEntity implements 
         super.saveAdditional(compound, provider);
         compound.putInt("CookTime", this.cookTime);
         compound.putInt("CookTimeTotal", this.cookTimeTotal);
+        compound.putBoolean("IsRecipeActive", this.isRecipeActive);
         ContainerHelper.saveAllItems(compound, this.items, provider);
     }
 
@@ -250,6 +257,7 @@ public class SolarCookerBlockEntity extends BaseContainerBlockEntity implements 
             if (isSunlit && !entity.items.get(INPUT).isEmpty()) {
                 RecipeHolder<? extends AbstractCookingRecipe> recipe = entity.getRecipe();
                 if (entity.canSmelt(level.registryAccess(), recipe)) {
+                    entity.isRecipeActive = true;
                     entity.cookTime++;
                     if (entity.cookTime == entity.cookTimeTotal) {
                         entity.cookTime = 0;
@@ -261,9 +269,12 @@ public class SolarCookerBlockEntity extends BaseContainerBlockEntity implements 
                     }
                 } else {
                     entity.cookTime = 0;
+                    entity.isRecipeActive = false;
                 }
             } else if (!isSunlit && entity.cookTime > 0) {
                 entity.cookTime = Mth.clamp(entity.cookTime - 2, 0, entity.cookTimeTotal);
+            } else {
+                entity.isRecipeActive = false;
             }
 
             boolean isBurning = entity.cookTime > 0;
@@ -287,8 +298,8 @@ public class SolarCookerBlockEntity extends BaseContainerBlockEntity implements 
     }
 
     public boolean shouldLidBeOpen(int numPlayersUsing) {
-        boolean recipeActive = this.cookTime > 0 || (this.canSmelt(this.getLevel().registryAccess(), getRecipe()));
-        return numPlayersUsing > 0 || recipeActive && this.isSunlit();
+        boolean recipeActive = this.isRecipeActive || (this.getLevel() != null && (this.canSmelt(this.getLevel().registryAccess(), getRecipe())));
+        return numPlayersUsing > 0 || (recipeActive && this.isSunlit());
     }
 
     public void updateShouldLidBeOpen(int numPlayersUsing) {
